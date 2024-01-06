913353a425973e0c15c1042437f2f7fd657a1f55
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-06 04:26:34 +0000

    importing modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
+import add_0
 if __name__ == "__main__":
-    import add_0
     a = 1
     b = 2
-    c = add_0.add(a, b)
-    print(f"{a} + {b} = {c}")
+    print(f"{a} + {b} = {add_0.add(a, b)}")
