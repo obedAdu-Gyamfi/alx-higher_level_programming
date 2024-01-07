306a1f8ecb30edb8c25827270ac5f7c731417527
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-07 22:34:33 +0000

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+# This is a module that prints a list
+# of integers
+
 def print_list_integer(my_list=[]):
+
+    """ This function prints a list of integers
+        The function loops over the lists
+        and prints out each member of the list
+    """
+
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
