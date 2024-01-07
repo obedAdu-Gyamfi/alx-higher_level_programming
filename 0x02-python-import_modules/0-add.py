5535b6a13c6a52f4331e06d4a841b8112f619f3b
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-07 22:38:15 +0000

    Hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+# This module imports the add_0 module
+# The add_0 function adds two numbers
+
 if __name__ == "__main__":
+    """ This module would not be imported if
+        __import__ or __import__ * is used to
+        import
+    """
     import add_0
     a = 1
     b = 2
