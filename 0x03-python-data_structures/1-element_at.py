d808442a53a39ec61965f5edebf71cf980be95b7
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-07 23:12:21 +0000

    task 1 idx

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# ----------------------
+
+# This is a python function that retrieves an
+# element from a list like in c.
+# ---------------------------
+
+def element_at(my_list, idx):
+
+    """ if idx is negetive, the function returns
+        None
+        Else if idx is out of range, the function returns
+        None
+        Else The function returns the element at index position
+        idx
+    """
+
+    if (idx < 0):
+        return (None)
+
+    elif (idx > len(my_list)):
+        return (None)
+    else:
+        return (my_list[idx])
