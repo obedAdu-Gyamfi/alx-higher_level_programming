36d4672daa6cdc684783229ed8e4e2e1fad5bfbe
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-08 01:42:31 +0000

    deletion at an index position

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -17,7 +17,9 @@ def delete_at(my_list=[], idx=0):
         new_list
     """
 
-    if ((len(my_list) < 0) or (idx >= len(my_list))):
+    if (len(my_list) < 0):
+        return (my_list)
+    elif (idx >= len(my_list)):
         return (my_list)
     else:
         del my_list[idx]
