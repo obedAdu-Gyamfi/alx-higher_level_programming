f60644fd6e2b4f004713bd6a19f5b6c38c935056
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-10 09:29:46 +0000

    Basic python arithmetic calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 # -------------------------
-
 # A python basic calculator
+# This module does not execute if imported with * or
+# __import__
+# (c) Obed Adu-Gyamfi
+# Email: adugyamfiobed.tpp3@gmail.com
 # ------------------------
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
 
 if __name__ == "__main__":
-    """ This module would not be executed if:
-        __import__ or * is used to import it.
-
-    """
-
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
-
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
