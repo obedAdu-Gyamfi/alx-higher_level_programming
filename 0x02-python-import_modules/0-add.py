b61ab95998e75f4e1e7185ec400cdeb4beadb050
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-10 09:44:07 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,10 +5,10 @@
 # The add_0 function adds two numbers
 # -----------------------------------
 
-import add_0
+from add_0 import add
 
 a = 1
 b = 2
 
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    print(f"{a} + {b} = {add(a, b)}")
