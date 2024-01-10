4101ae71061f4bd21d149cff6ac709fb2a4a347b
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2024-01-10 04:59:07 -0800

    my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+from sys import argv
+if __name__ == "__main__":
+    alist = []
+    for line in argv[1:]:
+        alist.append(line)
+
+    if (len(alist) != 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operator = alist[1]
+
+    if (operator == "+"):
+        a = int(alist[0])
+        b = int(alist[2])
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif (operator == '*'):
+        a = int(alist[0])
+        b = int(alist[2])
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif (operator == "-"):
+        a = int(alist[0])
+        b = int(alist[2])
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif (operator == "/"):
+        a = int(alist[0])
+        b = int(alist[2])
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
