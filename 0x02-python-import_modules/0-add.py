873c1eb4a25993e68b99667e80c3bbcd85039d63
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-10 11:02:01 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-# ----------------------------------
-# This module imports the add_0 module
 
-# The add_0 function adds two numbers
-# -----------------------------------
-
+from add_0 import add
 if __name__ == "__main__":
     """ code would not execute if import with * or __import__"""
-    from add_0 import add
+
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
