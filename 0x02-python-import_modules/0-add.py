181af737f71d69462ea0cc9be2c5b062dc9205a3
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-10 09:56:41 +0000

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,10 +5,9 @@
 # The add_0 function adds two numbers
 # -----------------------------------
 
-from add_0 import add
-
-a = 1
-b = 2
-
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    """ code would not execute if import with * or __import__"""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
