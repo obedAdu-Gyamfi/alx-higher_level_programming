14690c26c4f1283a671f762e21b5a5a33bef335a
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2024-01-10 04:02:53 -0800

    Okay

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-
-# Module imports the add_0 module to do a basic arithmetic
-# calculations
-# (c) Obed Adu-Gyamfi
-# ----------------------
-
 from add_0 import add
 if __name__ == "__main__":
-    """ Code does not execute if imported with * or __import__
-    """
-
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
