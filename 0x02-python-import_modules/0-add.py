4001a90e42622c9867951348ea4a60506683cf03
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-10 11:21:52 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+
+# Python basic Calculator
+
 from add_0 import add
+
 if __name__ == "__main__":
+    """ Code does not execute if imported with * or __import__
+    """
+
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
