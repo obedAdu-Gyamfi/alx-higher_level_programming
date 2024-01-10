c6ea112ba7dd5af1ef582cdb2c46f48758bd4baf
Obed <adugyamfiobed.tpp3@gmail.com>
2024-01-10 09:10:20 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -11,8 +11,4 @@ a = 1
 b = 2
 
 if __name__ == "__main__":
-    """ This module would not be imported if
-        __import__ or __import__ * is used to
-        import
-    """
     print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
